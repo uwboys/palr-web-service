657ac996717da4c2ca6e195d52e1e790eb523b32
Jobair Hassan <jobair.hassan96@gmail.com>
2016-10-15 17:09:58 -0400

    Restructures project

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
 # connect to local database
 app.config['MONGO_HOST'] = 'localhost'
 app.config['MONGO_PORT'] = 27017
 app.config['MONGO_DBNAME'] = 'local'
 mongo = PyMongo(app, config_prefix='MONGO')
 #mongo =  PyMongo(app)
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
 
 @app.route("/users")
 def list_users():
@@ -21,6 +17,28 @@ def list_users():
         print(document)
     print users
     return "Hello World!"
+
+@app.route('/login', methods=['POST'])
+def login():
+    json = request.get_json()
+    print json
+    return "hello"
+
+@app.route('/register', methods = ['POST'])
+def new_user():
+    username = request.get_json().get('username')
+    password = request.get_json().get('password')
+
+    if username is None or password is None:
+        abort(400) # missing arguments
+    if User.query.filter_by(username = username).first() is not None:
+        abort(400) # existing user
+
+    user = User(username = username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({ 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
 
 if __name__ == "__main__":
     app.run()
