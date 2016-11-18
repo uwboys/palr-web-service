2c8b2748f2b18443f6dba50472947f05e52fae86
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2016-11-18 00:59:51 -0500

    Trying heroku

---
+++ herokurunserver.py
@@ -0,0 +1,10 @@
+"""
+This script runs the application for Heroku.
+"""
+
+import os
+from PalrWebService import app
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
