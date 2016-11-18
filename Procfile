8d49717d78ffa92735c53f991d44b314c396353d
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2016-11-18 01:24:44 -0500

    Heroku please work

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python herokurunserver.py
+web: gunicorn -b 0.0.0.0:$PORT herokurunserver:app
--- herokurunserver.py
+++ herokurunserver.py
@@ -3,8 +3,12 @@ This script runs the application for Heroku.
 """
 
 import os
+
+from flask import Flask
 from PalrWebService import app
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+app = Flask(__name__)
+
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
